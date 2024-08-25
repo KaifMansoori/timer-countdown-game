b2a5070c100ee5db6a36a0ac4c7e6e4a71b93839
mod kaf <mhkaif7411@gmail.com>
2024-08-25 19:25:50 +0530

    update the createPorter

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { forwardRef,remainingTime, useImperativeHandle, useRef } from 'react';
+import {createPortal} from 'react-dom';
 
 const ResultModal= forwardRef(function ResultModal({remainingTime, targetTime}, ref) {
   const dialog= useRef();
@@ -13,14 +14,14 @@ const ResultModal= forwardRef(function ResultModal({remainingTime, targetTime}, 
       }
     }
   })
-  return (
+  return  createPortal(
     <dialog ref={dialog} className='result-modal'>
         {userLost && <h2>You Lost</h2>}
         {!userLost && <h2>Your Scroe: {score}</h2>}
         <p>The Target time was <strong>{targetTime} seconds.</strong></p>
         <p>You Stop the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
         <form method='dilaog'><button>Close</button></form>
-    </dialog>
+    </dialog>, document.getElementById('modal')
   )
 });
 
